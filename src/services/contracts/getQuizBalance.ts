3780040d45fcacea6b01c0bb506b7f341a9d068b
lucjdo <luciano.sejudo@gmail.com>
2023-11-19 00:33:05 +0100

    install rfh

--- src/services/contracts/getQuizBalance.ts
+++ src/services/contracts/getQuizBalance.ts
@@ -2,13 +2,13 @@ import { QUIZ_ABI } from '@services/utils/abis/quiz'
 import { QUIZ_CONTRACT_ADDRESS } from '@services/utils/addresses'
 import { Contract, ethers, formatUnits } from 'ethers'
 
-export async function getQuizBalance() {
+export async function getQuizBalance(address: string) {
   try {
     if (window.ethereum) {
       const provider = new ethers.BrowserProvider(window.ethereum)
       const contract = new Contract(QUIZ_CONTRACT_ADDRESS, QUIZ_ABI, provider)
-      const balance = await contract.balanceOf('ethers.eth')
-      return formatUnits(balance, 'wei')
+      const balance = await contract.balanceOf(address)
+      return formatUnits(balance)
     } else {
       alert('Install metamask extension!')
     }
