74039b37264572586b625b9e1f65d7519724918a
lucjdo <luciano.sejudo@gmail.com>
2023-11-18 10:43:19 +0100

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@containers': '/src/containers',
+      '@services': '/src/services',
+      '@hooks': '/src/hooks',
+      '@assets': '/src/assets'
+    }
+  }
 })
