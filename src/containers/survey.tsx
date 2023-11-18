3adabcc255c9bbcf96297d5b6b40f672396225a3
lucjdo <luciano.sejudo@gmail.com>
2023-11-18 20:40:13 +0100

    create survey quiz container

--- src/containers/survey.tsx
+++ src/containers/survey.tsx
@@ -1,10 +1,14 @@
 import { Stack } from '@mui/material'
 import SurveyWelcome from './survey-header'
+import SurveyQuiz from './survey-quiz'
+import { useAccount } from '@hooks/useAccount'
 
 export default function Survey() {
+  const { connected } = useAccount()
   return (
     <Stack>
       <SurveyWelcome />
+      {connected && <SurveyQuiz />}
     </Stack>
   )
 }
